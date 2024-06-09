bcb14b53d3340e688bc51254ad6e59f1cbf5aed5
Thelilrex <137387550+Thelilrex@users.noreply.github.com>
2024-06-09 11:34:21 +0900

    Changed the Database Url

--- Data/database.py
+++ Data/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from Settings.config import settings
 
-URL_DATABASE = "postgresql://postgres:@localhost:5432/Test"
+URL_DATABASE = settings.database_url
 engine = create_engine(URL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
